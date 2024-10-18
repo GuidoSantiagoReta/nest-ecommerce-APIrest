07a025905c6d74eeee917720c496c48d068aa584
GuidoSantiagoReta <guidosantiagoreta@gmail.com>
2024-10-18 11:38:38 -0300

    Se realiza tarea asíncrona

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OperadoresModule } from './operadores/operadores.module';
 import { ProductosModule } from './productos/productos.module';
+import { HttpModule, HttpService } from '@nestjs/axios'; 
+import { lastValueFrom } from 'rxjs';
 
 const APIKEY ='DEV-456';
 const APIKEYPROD = 'PROD-12345';
+const TAREA_ASINC = 'TAREA_ASINC';
 
 @Module({
-  imports: [OperadoresModule, ProductosModule],
+  imports: [HttpModule,OperadoresModule, ProductosModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: 'APIKEY',
       useValue: process.env.NODE_ENV === 'prod' ? APIKEYPROD : APIKEY,
-    }
+    },
+    {
+      provide: TAREA_ASINC,
+      useFactory: async (http: HttpService) => { 
+        const req = http.get('https://jsonplaceholder.typicode.com/posts')
+         const tarea = await lastValueFrom(req); 
+        return tarea.data;
+      },
+      inject: [HttpService],
+    },
 
+   
   ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,10 +2,15 @@ import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  // Inyectar la variable
-  constructor(@Inject('APIKEY') private apiKey: string) {} 
+
+  constructor(@Inject('APIKEY') private apiKey: string, @Inject('TAREA_ASINC') private tarea: any) {}
 
   getHello(): string {
     return `La llave de la aplicacion es: ${this.apiKey}`;
+  }
+
+  getUseFactory(): string {
+    console.log(this.tarea); 
+    return `Realizando una tarea asíncrona de ejemplo. La tarea es: ${JSON.stringify(this.tarea)}`;
   }
 }
